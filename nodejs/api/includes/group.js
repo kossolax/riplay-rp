48cfc21fbc90bf7a3bf0c9c04a539bc21e440964
Gozer26 <gozerdu26@gmail.com>
2020-09-12 09:47:51 +0200

    Modif quota gang 10 > 6

--- nodejs/api/includes/group.js
+++ nodejs/api/includes/group.js
@@ -171,8 +171,8 @@ server.put('/group/:groupid/:steamid', function (req, res, next) {
               return res.send(new ERR.ForbiddenError("Vous n'avez pas les permissions suffisantes pour ce rang."));
 
           server.conn.query("SELECT COUNT(`group_id`) as `cpt` FROM `rp_users` U INNER JOIN `rp_groups` G ON G.`id`=U.`group_id` WHERE (G.`id`=? OR G.`owner`=?) AND U.`steamid`<>? ;", [uniqID, uniqID, req.params["steamid"]], function(err, row) {
-            if( jobID != 0 && parseInt(row[0].cpt) >= 10 )
-              return res.send(new ERR.ForbiddenError("Impossible de recruter plus de 10 personnes dans votre groupe."));
+            if( jobID != 0 && parseInt(row[0].cpt) >= 6 )
+              return res.send(new ERR.ForbiddenError("Impossible de recruter plus de 6 personnes dans votre groupe."));
 
             server.conn.query("UPDATE `rp_users` SET `group_id`=? WHERE `steamid`=? LIMIT 1;", [jobID, req.params["steamid"]], function(err, row) {
               if( err ) return res.send(new ERR.InternalServerError(err));
