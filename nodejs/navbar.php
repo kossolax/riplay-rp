5e9b6c6c038bd8477c4c2ecd7597a0cf9a5d93d5
Steve ZARETTI <kossolax@gmail.com>
2021-01-02 17:21:06 +0100

    sync

---
+++ nodejs/navbar.php
@@ -0,0 +1,57 @@
+<nav class="navbar navbar-default" role="navigation">
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar" style="z-index:1;">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+		</button>
+		<!-- <a class="navbar-brand" href="/#/"><img src="./images/bleue42.png" alt="Riplay" width="350" height="100" style="max-height:100px !important"/></a> -->
+	</div>
+	<div id="navbar" class="navbar-collapse collapse">
+	<ul class="nav navbar-nav navbar-right" style="z-index:2;">
+			<li class="visible-lg-block"><a href="#/">Accueil</a> </li>
+			<li class="dropdown">
+				<a class="hidden-md hidden-lg" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Le Site <span class="caret"></span></a>
+				<a class="hidden-xs hidden-sm" href="/#" >Le Site <span class="caret"></span></a>
+
+				<ul class="dropdown-menu">
+						<li><a class="hidden-md hidden-lg" href="/#"><i class="fa fa-home"></i> Accueil</a></li>
+						<li><a href="#/pilori/view/{{steamid}}"><i class="fa fa-ban"></i> Ban-liste</a></li>
+					<!-- <li><a href="/index.php?page=intro&amp;last=news"><i class="fa fa-newspaper-o"></i> News</a></li> -->
+					<!-- <li><a href="/forum/memberlist.php?g=19&amp;mode=group&amp;sk=m&amp;sd=a"><i class="fa fa-users"></i> Les membres</a></li>
+					<li><a class="hidden-xs" href="/index.php?page=valider_steamid"><i class="fa fa-steam"></i> Modifier son Steam</a></li>
+					<li><a href="/index.php?page=money"></a><a href="/index.php?page=serveurs"><i class="fa fa-server"></i> Info-server<span></span></a></li>
+					<li><a href="/index.php?page=money"><i class="fa fa-usd"></i> Achat de $RP</a></li>-->
+				</ul>
+			</li>
+			
+			<li class="dropdown">
+				<a class="hidden-md hidden-lg" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">RolePlay <span class="caret"></span></a>
+				<a class="hidden-xs hidden-sm" href="#" >RolePlay <span class="caret"></span></a>
+				<ul class="dropdown-menu">
+					<li><a href="https://forum.riplay.fr/index.php?/forum/34-besoin-dun-admin/" target="_blank"><i class="fa fa-wikipedia-w"></i> Besoin d'aide?</a></li>
+					<li><a href="https://forum.riplay.fr/index.php?/forum/60-r%C3%A8glements-roleplay/" target="_blank"><i class="fa fa-balance-scale"></i> Règlement général</a></li>
+					<!-- <li><a href="/index.php?page=roleplay2#/"><i class="fa fa-briefcase"></i> Liste des jobs<span></span></a></li> -->
+					<li><a href="#/success/{{ steamid }}"><i class="fa fa-trophy"></i> Les succès</a></li>
+					<li><a class="hidden-xs" href="https://rpweb.riplay.fr/index.php#/tribunal/report"><i class="fa fa-gavel"></i> Le Tribunal</a></li>
+					<li><a href="#/parrainage"><i class="fa fa-user-plus"></i> Parrainage</a></li>
+                                        <li><a class="hidden-xs" href="#/craft/0"><i class="fa fa-user-plus"></i> Crafting</a></li>
+					<li><a href="#/donation"><i class="fa fa-usd"></i> Achat de $RP</a></li>
+				</ul>
+			</li>
+			<li class="dropdown"><a href="/index.php?page=aide">Aide <span class="caret hidden-xs"></span></a>
+				<ul class="dropdown-menu">
+					<li><a href="/index.php?page=aide&sub=debuter"><i class="fa fa-key"></i> Comment bien débuter?</a></li>
+					<li><a href="/index.php?page=aide&sub=emploi"><i class="fa fa-suitcase"></i> Comment trouver un job?</a></li>
+					<li><a href="/index.php?page=aide&sub=argent"><i class="fa fa-money"></i> Comment gagner de l'argent?</a></li>
+					<li><a href="/index.php?page=aide&sub=bind"><i class="fa fa-keyboard-o"></i> Mettre en place vos binds</a></li>
+					<li><a href="/index.php?page=aide&sub=nopyj"><i class="fa fa-ticket"></i> Qu'est-ce que le rang no-pyj ?</a></li>
+                    <li><a href="/forum/viewtopic.php?p=640496"><i class="fa fa-flask"></i> Qu'est-ce que l'artisanat ?</a></li>
+				</ul>
+			</li>
+
+			<li class="dropdown">
+				<li class="visible-lg-block"><a href="http://forum.riplay.fr">Forum</a> </li>
+			</li>
+		</ul>
+	</div>
+</nav>
